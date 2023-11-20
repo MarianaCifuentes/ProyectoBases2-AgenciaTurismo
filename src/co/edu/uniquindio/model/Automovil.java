7c55114228bd163c3b0330a21c6791adaefe7e82
ThiagoMaya <113477962+ThiagoMaya@users.noreply.github.com>
2023-11-20 11:49:51 -0500

    Se añadieron archivos al paquete modelo

---
+++ src/co/edu/uniquindio/model/Automovil.java
@@ -0,0 +1,124 @@
+package co.edu.uniquindio.Modelo;
+
+public class Automovil {
+
+    private int id;
+    private int modelo;
+    private int valor_Renta;
+    private int valor_Adicional;
+    private String placa;
+    private String marca;
+    private String tipo;
+    private String gama;
+
+
+    public Automovil() {
+        super();
+    }
+
+
+    public Automovil(int id, int modelo, int valor_Renta, int valor_Adicional, String placa, String marca, String tipo,
+            String gama) {
+        this.id = id;
+        this.modelo = modelo;
+        this.valor_Renta = valor_Renta;
+        this.valor_Adicional = valor_Adicional;
+        this.placa = placa;
+        this.marca = marca;
+        this.tipo = tipo;
+        this.gama = gama;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    public int getModelo() {
+        return modelo;
+    }
+
+
+    public void setModelo(int modelo) {
+        this.modelo = modelo;
+    }
+
+
+    public int getValor_Renta() {
+        return valor_Renta;
+    }
+
+
+    public void setValor_Renta(int valor_Renta) {
+        this.valor_Renta = valor_Renta;
+    }
+
+
+    public int getValor_Adicional() {
+        return valor_Adicional;
+    }
+
+
+    public void setValor_Adicional(int valor_Adicional) {
+        this.valor_Adicional = valor_Adicional;
+    }
+
+
+    public String getPlaca() {
+        return placa;
+    }
+
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+
+    public String getMarca() {
+        return marca;
+    }
+
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+
+    public String getTipo() {
+        return tipo;
+    }
+
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+
+    public String getGama() {
+        return gama;
+    }
+
+
+    public void setGama(String gama) {
+        this.gama = gama;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Automovil [id=" + id + ", modelo=" + modelo + ", valor_Renta=" + valor_Renta + ", valor_Adicional="
+                + valor_Adicional + ", placa=" + placa + ", marca=" + marca + ", tipo=" + tipo + ", gama=" + gama + "]";
+    }
+
+    
+
+
+    
+    
+}
---
+++ src/co/edu/uniquindio/model/ReservaAutomovil.java
@@ -0,0 +1,97 @@
+package co.edu.uniquindio.Modelo;
+
+import java.time.LocalDate;
+
+public class ReservaAutomovil {
+
+    private Integer id;
+    private int idCliente;
+    private int agencia_automovil_id;
+    private LocalDate fecha_reserva;
+    private LocalDate fecha_entrega;
+    private int valor_renta;
+    private int valor_adicional;
+
+    public ReservaAutomovil() {
+        super();
+    }
+
+    public ReservaAutomovil(int id, int idCliente, int agencia_automovil_id, LocalDate fecha_reserva, LocalDate fecha_entrega,
+            int valor_renta, int valor_adicional) {
+        this.id = id;
+        this.idCliente = idCliente;
+        this.agencia_automovil_id = agencia_automovil_id;
+        this.fecha_reserva = fecha_reserva;
+        this.fecha_entrega = fecha_entrega;
+        this.valor_renta = valor_renta;
+        this.valor_adicional = valor_adicional;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public int getAgencia_automovil_id() {
+        return agencia_automovil_id;
+    }
+
+    public void setAgencia_automovil_id(int agencia_automovil_id) {
+        this.agencia_automovil_id = agencia_automovil_id;
+    }
+
+    public LocalDate getFecha_reserva() {
+        return fecha_reserva;
+    }
+
+    public void setFecha_reserva(LocalDate fecha_reserva) {
+        this.fecha_reserva = fecha_reserva;
+    }
+
+    public LocalDate getFecha_entrega() {
+        return fecha_entrega;
+    }
+
+    public void setFecha_entrega(LocalDate fecha_entrega) {
+        this.fecha_entrega = fecha_entrega;
+    }
+
+    public int getValor_renta() {
+        return valor_renta;
+    }
+
+    public void setValor_renta(int valor_renta) {
+        this.valor_renta = valor_renta;
+    }
+
+    public int getValor_adicional() {
+        return valor_adicional;
+    }
+
+    public void setValor_adicional(int valor_adicional) {
+        this.valor_adicional = valor_adicional;
+    }
+
+    @Override
+    public String toString() {
+        return "ReservaAutomovil [id=" + id + ", idCliente=" + idCliente + ", agencia_automovil_id="
+                + agencia_automovil_id + ", fecha_reserva=" + fecha_reserva + ", fecha_entrega=" + fecha_entrega
+                + ", valor_renta=" + valor_renta + ", valor_adicional=" + valor_adicional + "]";
+    }
+
+    
+
+    
+    
+}
